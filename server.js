53b6790429c5e102958db85f8cf004fb31aaefb6
Haley Telega <haleytelega@outlook.com>
2022-04-25 14:49:01 -0400

    added port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001; //Tell Heroku to use that port
 const app = express();
 const { animals } = require('./data/animals.json'); //use to require data
 
@@ -48,6 +49,6 @@ app.get('/api/animals', (req, res) => { //route
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
